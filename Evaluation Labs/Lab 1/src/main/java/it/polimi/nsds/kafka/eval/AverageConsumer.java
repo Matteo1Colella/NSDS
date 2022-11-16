5a9cac30e4774d08d57beaf4221e2784be4ed185
mirkobitetto <63520965+mirkobitetto@users.noreply.github.com>
2022-11-16 10:09:03 +0100

    Add files via upload

---
+++ Evaluation Labs/Lab 1/src/main/java/it/polimi/nsds/kafka/eval/AverageConsumer.java
@@ -0,0 +1,70 @@
+package it.polimi.nsds.kafka.eval;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class AverageConsumer {
+    private static final String serverAddr = "localhost:9092";
+    private static final String inputTopic = "outputTopic";
+
+    private static final boolean autoCommit = true;
+    private static final int autoCommitIntervalMs = 15000;
+
+    // Default is "latest": try "earliest" instead
+    private static final String offsetResetStrategy = "latest";
+
+    public static void main(String[] args) {
+        // final String consumerGroupId = args[0];
+        final String consumerGroupId = "GroupA";
+
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, serverAddr);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, String.valueOf(autoCommit));
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, String.valueOf(autoCommitIntervalMs));
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetResetStrategy);
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
+
+        KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(inputTopic));
+
+        // New hashmap to store the last value of each key
+        HashMap<String, Integer> lastValues = new HashMap<String, Integer>();
+
+        while (true) {
+            final ConsumerRecords<String, Integer> records = consumer.poll(Duration.of(5, ChronoUnit.MINUTES));
+            for (final ConsumerRecord<String, Integer> record : records) {
+
+                // Add the last value on the hashmap
+                lastValues.put(record.key(), record.value());
+
+                int size = lastValues.size();
+                int sum = lastValues.values().stream().mapToInt(Integer::intValue).sum();
+
+                float average = (float) sum / size;
+
+                System.out.print("Consumer group: " + consumerGroupId + "\t");
+                System.out.println("Partition: " + record.partition() +
+                        "\tOffset: " + record.offset() +
+                        "\tKey: " + record.key() +
+                        "\tValue: " + record.value() +
+                        "\tCurrent Average: " + average);
+
+            }
+        }
+    }
+}
